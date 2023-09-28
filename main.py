c67a97613040d7012a861ad5b7a193784670e6b3
pvs3 <35466280+pvs3@users.noreply.github.com>
2023-09-28 18:20:05 +0200

    pxt.json, main.blocks, main.ts, main.py bijwerken

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 getal = 0
+status = 0
+pin = 0
+pinA = 0
 rij = 0
 kol = 0
+def dump(num: number, num2: number):
+    serial.write_value("x", num)
+    serial.write_value("y", num2)
+    serial.write_line("---------")
 
 def on_button_pressed_a():
     basic.show_number(randint(1, 6))
@@ -10,10 +17,21 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_gesture_shake():
     basic.clear_screen()
-    for index in range(25):
-        Toongetal(index)
+    for index in range(101):
+        led.toggle(4, 0)
         basic.pause(100)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_pin_pressed_p2():
+    global getal
+    serial.write_number(0)
+    serial.write_line("")
+    music.play(music.tone_playable(262, music.beat(BeatFraction.SIXTEENTH)),
+        music.PlaybackMode.UNTIL_DONE)
+    getal += -1
+    basic.clear_screen()
+    Toongetal(getal)
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
 
 def on_button_pressed_ab():
     global getal
@@ -39,42 +57,55 @@ def on_button_pressed_ab():
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
-    global getal
+    global getal, status, pin, pinA
     getal = 5
     for index2 in range(5):
         basic.show_number(getal)
         getal += -1
         basic.pause(100)
     basic.pause(100)
-    basic.clear_screen()
-    getal = 24
+    getal = 19
     Toongetal(getal)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_pin_pressed_p1():
-    global getal
-    while input.pin_is_pressed(TouchPin.P1):
-        pass
+    status = 0
+    while getal >= 0:
+        pin = pins.digital_read_pin(DigitalPin.P1)
+        if pin == 1:
+            if status == 1:
+                getal += -1
+                serial.write_number(pins.analog_read_pin(AnalogPin.P1))
+                serial.write_line("")
+                Toongetal(getal)
+                music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+                    music.PlaybackMode.UNTIL_DONE)
+                status = 0
+                led.unplot(4, 0)
+        else:
+            status = 1
+            led.plot(4, 0)
+            pinA = pins.analog_read_pin(AnalogPin.P1)
+            serial.write_number(pinA)
+            serial.write_line("")
+            if pinA < 3:
+                basic.show_icon(IconNames.HAPPY)
+                music.play(music.string_playable("C D E F G A B C5 ", 120),
+                    music.PlaybackMode.UNTIL_DONE)
+                pinA = 100
     music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.UNTIL_DONE)
-    getal += -1
-    serial.write_number(getal)
-    serial.write_line("")
-    Toongetal(getal)
-input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+    basic.show_icon(IconNames.NO)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def Toongetal(num: number):
+def Toongetal(num3: number):
     global rij, kol
-    rij = num / 5
-    kol = num % 5
-    index3 = 0
-    while index3 <= rij:
-        serial.write_value("x", index3)
-        serial.write_string("")
+    basic.clear_screen()
+    rij = Math.floor(num3 / 5)
+    kol = num3 % 5
+    for index3 in range(5):
         index22 = 0
-        while index22 <= kol:
-            serial.write_value("y", index22)
-            led.plot(index22, index3)
+        while index22 <= rij - 1:
+            led.plot(index3, 4 - index22)
             index22 += 1
-        serial.write_line("")
-        index3 += 1
+    index4 = 0
+    while index4 <= kol:
+        led.plot(index4, 4 - rij)
+        index4 += 1
--- main.ts
+++ main.ts
@@ -1,54 +1,12 @@
+let getal = 0
 let rij = 0
 let kol = 0
-let getal = 0
+function dump (num: number, num2: number) {
+    serial.writeValue("x", num)
+    serial.writeValue("y", num2)
+    serial.writeLine("---------")
+}
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(randint(1, 6))
-    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
-    for (let index = 0; index <= 0; index++) {
-        basic.clearScreen()
-        Toongetal(24 - index)
-        serial.writeValue("getal", index)
-        serial.writeString("")
-        serial.writeValue("x", rij)
-        serial.writeString("")
-        serial.writeValue("y", kol)
-        serial.writeLine("")
-        basic.pause(500)
-    }
-    basic.clearScreen()
-    for (let index2 = 0; index2 <= 4; index2++) {
-        for (let index = 0; index <= 4; index++) {
-            led.plot(index, 4 - index2)
-            basic.pause(500)
-        }
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    getal = randint(1, 3)
-    if (getal == 1) {
-        basic.showIcon(IconNames.Square)
-    } else if (getal == 2) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
-    } else {
-        basic.showLeds(`
-            . . . # #
-            # . # . .
-            . # . . .
-            # . # . .
-            . . . # #
-            `)
-    }
-})
-input.onButtonPressed(Button.B, function () {
     getal = 5
     for (let index = 0; index < 5; index++) {
         basic.showNumber(getal)
@@ -56,27 +14,34 @@ input.onButtonPressed(Button.B, function () {
         basic.pause(100)
     }
     basic.pause(100)
-    basic.clearScreen()
-    getal = 24
+    getal = 19
     Toongetal(getal)
 })
-input.onPinPressed(TouchPin.P1, function () {
-    while (input.pinIsPressed(TouchPin.P1)) {
-    	
-    }
-    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+input.onPinPressed(TouchPin.P2, function () {
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
     getal += -1
-    serial.writeNumber(getal)
-    serial.writeLine("")
-    basic.clearScreen()
     Toongetal(getal)
+    if (getal == 0) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        basic.showIcon(IconNames.No)
+        while (true) {
+        	
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(20 - getal)
 })
 function Toongetal (num: number) {
+    basic.clearScreen()
     rij = Math.floor(num / 5)
     kol = num % 5
-    for (let index = 0; index <= rij - 0; index++) {
-        for (let index2 = 0; index2 <= kol; index2++) {
+    for (let index = 0; index <= 4; index++) {
+        for (let index2 = 0; index2 <= rij - 1; index2++) {
             led.plot(index, 4 - index2)
         }
+    }
+    for (let index = 0; index <= kol; index++) {
+        led.plot(index, 4 - rij)
     }
 }
